02a6ad77f821a5229449dd9097058c848fd13ec5
owais5975 <79499299+owais5975@users.noreply.github.com>
2022-09-10 21:24:39 +0500

    removal

--- Hotel.API/Controllers/HotelController.cs
+++ Hotel.API/Controllers/HotelController.cs
@@ -18,9 +18,6 @@ namespace Hotel.API.Controllers
         [HttpGet("GetHotel")]
         public IActionResult GetHotel()
         {
-            var data = _context.Hotels.Where(x => x.Id == 7).ToList();
-            _context.Hotels.RemoveRange(data);
-            _context.SaveChanges();
             return Ok(new {Status = true, Message = _context.Hotels.ToList()});
         }
 
